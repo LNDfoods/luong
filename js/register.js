a7af696579e6b4fdae917a1ceafa8b71f6bdf8c5
LNDfoods <167158780+LNDfoods@users.noreply.github.com>
2024-04-22 15:22:29 +0700

    Update register.js
    update new method

--- js/register.js
+++ js/register.js
@@ -1,19 +1,13 @@
-/**
- * @file register.js
- * @author Sanjay Sunil
- * @license MIT
- */
+function registerUser(username, password) {
+  var hashedPassword = bcrypt.hashSync(password, 10); // Mã hóa mật khẩu
 
-function registration() {
-  console.log('Đang đăng ký người dùng ...')
-  var user_email = document.getElementById("user_email").value;
-  var user_password = document.getElementById("user_password").value;
-  var confirm_password = document.getElementById("confirm_password").value;
-  if (user_password !== confirm_password) {
-    errorNotification("Mật khẩu không trùng khớp!")
-  } else {
-    const auth = firebase.auth();
-    const promise = auth.createUserWithEmailAndPassword(user_email, user_password);
-    promise.catch((err) => errorNotification(err.message))
-  }
+  db.collection("Users").doc(username).set({
+    password: hashedPassword,
+    username: 
+    // thêm các thông tin khác như email, tên, v.v.
+  }).then(() => {
+    console.log("User registered successfully");
+  }).catch((error) => {
+    console.error("Error writing document: ", error);
+  });
 }
